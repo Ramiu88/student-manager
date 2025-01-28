709f4e6b3d85b2385eec3ed7bb90ccb5bb4f4562
Marwane Amal <123182415+Ramiu88@users.noreply.github.com>
2025-01-28 10:27:33 +0100

    Modify UML
    Remove static function in gestionnaire class.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,52 @@
 #include<string>
 
 
+using namespace std;
+
+
+class personne
+{
+  protected:
+
+  string nom;
+  int age;
+
+  public:
+
+  virtual void afficher() = 0;
+  virtual string obtenir_role() = 0;
+};
+
+
+class cours
+{
+   private:
+
+   string id_cours;
+   string nom;
+
+   public:
+
+   
+};
+
+
+class etudiant : public personne
+{
+   private:
+
+   string id_etudiant;
+   vector <cours> etudiant_cours;
+
+
+   public:
+
+
+};
+
+
+
+
 
 
 
