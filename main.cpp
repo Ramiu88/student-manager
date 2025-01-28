b53401bf9ea72e18b9961994376244382b2154ef
Marwane Amal <123182415+Ramiu88@users.noreply.github.com>
2025-01-28 11:21:52 +0100

    Modify UML
    Adjust diagram to use normal object decrarations instead of pointers.

--- main.cpp
+++ main.cpp
@@ -26,9 +26,19 @@ class cours
    private:
 
    string id_cours;
-   string nom;
+   string nom_cours;
 
    public:
+
+   string& get_id_cours()
+   {
+     return id_cours;
+   }
+
+   string& get_nom()
+   {
+     return nom_cours;
+   }
 
    
 };
@@ -43,6 +53,44 @@ class etudiant : public personne
 
 
    public:
+
+   etudiant(string nom,int age,string id_etudiant)
+   {
+     this->id_etudiant = id_etudiant;
+     this->nom = nom;
+     this->age = age;
+
+   }
+
+   etudiant(string nom,int age)
+   {
+     this->nom = nom;
+     this->age = age;
+
+   }
+
+   void afficher()
+   {
+     cout << "Id de l'etudiant est : "<<id_etudiant<<endl;
+     cout << "Le nom de l'etudiant est : "<<nom<<endl;
+     cout << "L'age de l'etudiant est : "<<age<<endl;
+     for(int i = 0;i < etudiant_cours.size();i++)
+     {
+        cout << "L'id de cours est : "<<etudiant_cours[i].get_id_cours()<<endl;
+        cout << "Le nom du cours est : "<<etudiant_cours[i].get_nom()<<endl;
+     }
+   }
+
+   string obtenir_role()
+   {
+      return "Etudiant";
+   }
+
+
+   void inscrire_cours(cours user_cours)
+   {
+      
+   }
 
 
 };
